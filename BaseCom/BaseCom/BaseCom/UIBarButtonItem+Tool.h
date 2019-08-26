f346b8d4ca662af6af94715e61f0a8794bee023f
mac <1174724547@qq.com>
2019-08-26 17:42:40 +0800

    增加了没有点击事件的barbutton

--- BaseCom/BaseCom/BaseCom/UIBarButtonItem+Tool.h
+++ BaseCom/BaseCom/BaseCom/UIBarButtonItem+Tool.h
@@ -18,6 +18,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIBarButtonItem (Tool)
 
+///获取barBtn
 +(instancetype)RWN_BarButnWithFrame:(CGRect)frame
                               title:(nullable NSString*)title
                          titleColor:(nullable UIColor *)titleColor
@@ -27,20 +28,54 @@ NS_ASSUME_NONNULL_BEGIN
                              action:(SEL)action
                           direction:(RWNBar)direction;
 
-
+///获取只有文字的barBtn
 +(instancetype)RWN_BarButnWithFrame:(CGRect)frame
                               title:(NSString*)title
                          titleColor:(UIColor *)titleColor
                                font:(UIFont *)font
                              Target:(id)target
-                             action:(SEL)action;
+                             action:(SEL)action
+                        direction:(RWNBar)direction;
 
+///获取只有文字的barBtn，不用写frame
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font
+                             Target:(id)target
+                             action:(SEL)action
+                          direction:(RWNBar)direction;
 
+///获取只有文字的barBtn，不用写frame,没有点击事件
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font
+                          direction:(RWNBar)direction;
+
+///获取只有文字的barBtn，不用写frame,没有点击事件,默认居中
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font;
+
+ ///获取只有图片的barBtn
 +(instancetype)RWN_BarButnWithFrame:(CGRect)frame
                               image:(UIImage *)image
                              Target:(id)target
                              action:(SEL)action
                           direction:(RWNBar)direction;
+
+///获取只有图片的barBtn，不用写frame
++(instancetype)RWN_BarButnWithimage:(UIImage *)image
+                             Target:(id)target
+                             action:(SEL)action
+                          direction:(RWNBar)direction;
+
+///获取只有图片的barBtn，不用写frame,没有点击事件
++(instancetype)RWN_BarButnWithimage:(UIImage *)image
+                          direction:(RWNBar)direction;
+
+
+///获取只有图片的barBtn，不用写frame,没有点击事件,默认居中
++(instancetype)RWN_BarButnWithimage:(UIImage *)image;
 
 @end
 
--- BaseCom/BaseCom/BaseCom/UIBarButtonItem+Tool.m
+++ BaseCom/BaseCom/BaseCom/UIBarButtonItem+Tool.m
@@ -25,34 +25,99 @@
     
 }
 
+
+
+///获取只有文字的barBtn
++(instancetype)RWN_BarButnWithFrame:(CGRect)frame
+                              title:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font
+                             Target:(id)target
+                             action:(SEL)action
+                          direction:(RWNBar)direction{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:frame title:title titleColor:titleColor image:nil font:font Target:target action:action direction:direction];
+    
+}
+
+///获取只有文字的barBtn，不用写frame
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font
+                             Target:(id)target
+                             action:(SEL)action
+                          direction:(RWNBar)direction{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:title titleColor:titleColor image:nil font:font Target:target action:action direction:direction];
+    
+}
+
+///获取只有文字的barBtn，不用写frame,没有点击事件
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font
+                          direction:(RWNBar)direction{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:title titleColor:titleColor image:nil font:font Target:nil action:nil direction:direction];
+
+}
+
+///获取只有文字的barBtn，不用写frame,没有点击事件,默认居中
++(instancetype)RWN_BarButnWithtitle:(NSString*)title
+                         titleColor:(UIColor *)titleColor
+                               font:(UIFont *)font{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:title titleColor:titleColor image:nil font:font Target:nil action:nil direction:RWNBarMiddle];
+
+}
+
+///获取只有图片的barBtn
 +(instancetype)RWN_BarButnWithFrame:(CGRect)frame
                               image:(UIImage *)image
                              Target:(id)target
                              action:(SEL)action
                           direction:(RWNBar)direction{
     
-    return  [UIBarButtonItem RWN_BarButnWithFrame:frame title:nil titleColor:nil image:image font:nil Target:target action:action direction:direction];
+    return [UIBarButtonItem RWN_BarButnWithFrame:frame title:nil titleColor:nil image:image font:nil Target:target action:action direction:direction];
     
 }
 
-
-+(instancetype)RWN_BarButnWithFrame:(CGRect)frame
-                              title:(NSString*)title
-                         titleColor:(UIColor *)titleColor
-                               font:(UIFont *)font
+///获取只有图片的barBtn，不用写frame
++(instancetype)RWN_BarButnWithimage:(UIImage *)image
                              Target:(id)target
-                             action:(SEL)action{
+                             action:(SEL)action
+                          direction:(RWNBar)direction{
     
-    return [UIBarButtonItem RWN_BarButnWithFrame:frame title:title titleColor:titleColor image:nil font:font Target:target action:action direction:RWNBarMiddle];
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:nil titleColor:nil image:image font:nil Target:target action:action direction:direction];
+
 }
+
+///获取只有图片的barBtn，不用写frame,没有点击事件
++(instancetype)RWN_BarButnWithimage:(UIImage *)image
+                          direction:(RWNBar)direction{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:nil titleColor:nil image:image font:nil Target:nil action:nil direction:direction];
+
+    
+}
+
+
+///获取只有图片的barBtn，不用写frame,没有点击事件,默认居中
++(instancetype)RWN_BarButnWithimage:(UIImage *)image{
+    
+    return [UIBarButtonItem RWN_BarButnWithFrame:CGRectMake(0, 0, 60, 44) title:nil titleColor:nil image:image font:nil Target:nil action:nil direction:RWNBarMiddle];
+
+    
+}
+
 
 +(UIButton *)getCustomeBtnWithFrame:(CGRect)frame
                               title:(nullable NSString*)title
                          titleColor:(nullable UIColor *)titleColor
                               image:(nullable UIImage *)image
                                font:(nullable UIFont *)font
-                             Target:(id)target
-                             action:(SEL)action
+                             Target:(nullable id)target
+                             action:(nullable SEL)action
                           direction:(RWNBar)direction{
     
     UIButton *barButton = [[UIButton alloc]initWithFrame:frame];
@@ -65,7 +130,9 @@
     if (titleColor) {
         [barButton setTitleColor:titleColor forState:UIControlStateNormal];
     }
-    [barButton addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    if (target) {
+        [barButton addTarget:target action:action forControlEvents:UIControlEventTouchUpInside];
+    }
     if (image) {
         [barButton setImage:image forState:UIControlStateNormal];
     }
